2f97ec7180fb13b36e85e8c475219401271eda45
Zidane <trey.gaines@navagis.com>
2012-08-14 11:24:55 -0500

    adding joe's changes to github

---
+++ trey.joe/index.php
@@ -0,0 +1,89 @@
+<?php
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/default.css" rel="stylesheet" type="text/css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script src="../jq/jquery-ui/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="../jq/jquery-ui/js/jquery-ui-1.8.22.custom.min.js" type="text/javascript"></script>
+<script src="../jq/jcoverflip-1.0.2/jquery.jcoverflip.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<div id="container">
+  <header id="top">
+    <h1>Header </h1>
+    <div id="logo"></div>
+    <!-- end #logo -->
+    <nav></nav>
+  </header>
+  <section>
+    <article>
+      <header class="article_header noBorder">
+        <div id="slideshow">
+        </div>
+      </header>
+      <footer class="article_footer noBorder"></footer>
+    </article>
+    <div class="topDiv"><a href="#top" class="topButton blockCenter">Top</a></div>
+  </section>
+  <!-- section1 slideShow -->
+  <section>
+    <article>
+      <header class="article_header noBorder">
+        <h1>About this website and it's contents</h1>
+      </header>
+      <footer class="article_footer noBorder"></footer>
+    </article>
+    <aside class="floatRight">
+      <h1>Tweets</h1>
+    </aside>
+    <!-- latestTweets <footer></footer>-->
+    <div class="topDiv"><a href="#top" class="topButton blockCenter">Top</a></div>
+  </section>
+  <!-- section2 webDesign and Development -->
+  <section>
+    <article>
+      <header class="article_header noBorder">
+        <h1>Words About Us</h1>
+      </header>
+      <footer class="article_footer noBorder"></footer>
+    </article>
+    <aside class="floatRight">
+      <h1>Skills & Awards</h1>
+    </aside>
+    <!--skills&awards-->
+    <div class="topDiv"><a href="#top" class="topButton blockCenter">Top</a></div>
+  </section>
+  <!-- wordsAboutUs -->
+  <section>
+    <article>
+      <header class="article_header noBorder">
+        <h1>Works</h1>
+      </header>
+      <footer class="article_footer noBorder"></footer>
+    </article>
+    <div class="topDiv"><a href="#top" class="topButton blockCenter">Top</a></div>
+  </section>
+  <!-- works -->
+  <section>
+  <article>
+  <header class="article_header noBorder"><h1>How To Find Us</h1></header>
+    <footer class="article_footer noBorder"></footer>
+    </article>
+    <div class="topDiv"><a href="#top" class="topButton blockCenter">Top</a></div>
+  </section>
+  <!-- howToFindUs & getInTouch -->
+  <footer>
+    <h1>footer</h1>
+  </footer>
+</div>
+<!-- end #container -->
+</body>
+</html>
